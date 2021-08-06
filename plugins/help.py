33e6da3342d0d546893d7c25972fa66f5018599f
ytdladarsh <88391761+ytdladarsh@users.noreply.github.com>
2021-08-06 14:02:38 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url   always choose the best quality for better result"
+    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url   {message.from_user.first_name} always choose the best quality for better result(Under 2GB)"
     await message.reply_text(helptxt)
